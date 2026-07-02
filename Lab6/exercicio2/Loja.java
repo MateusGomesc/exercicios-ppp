67618310c4a4110dac32c5ab7bbb0c1b192e72b6
Mateus Gomes <129867943+MateusGomesc@users.noreply.github.com>
2026-07-02 10:10:04 -0300

    Update Loja.java

--- Lab6/exercicio2/Loja.java
+++ Lab6/exercicio2/Loja.java
@@ -1,6 +1,8 @@
 package Lab6.exercicio2;
 
 public class Loja {
+    //O padrão Template Method foca em definir a estrutura fixa de um algoritmo em uma classe base delegando passos específicos para subclasses, 
+    //enquanto o Decorator foca em adicionar dinamicamente novas responsabilidades e comportamentos a um objeto existente em tempo de execução sem alterar sua estrutura.
     public static void main(){
         Item i1 = new Item("Monitor LG", 899.90);
         Item i2 = new Item("Mouse Logitech", 69.90);
